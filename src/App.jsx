38316ce70aa94cd4bf762ce39f65fc7a528dbfd0
g_sampao <gnsampao@gmail.com>
2024-04-12 14:37:06 +0300

    Page structure completed. First commit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { iconStar } from "./assets/images";
+import Widget from "./components/Widget";
+import { details } from "./constants";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
+    <section>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={iconStar} alt="icon-star" />
+        <h1>FAQs</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        {details.map((detail) => (
+          <Widget
+            key={detail.key}
+            summary={detail.summary}
+            content={detail.content}
+          />
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </section>
+  );
+};
 
-export default App
+export default App;
