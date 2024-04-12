a8ffcf372514443b321576db941790beea8d500a
g_sampao <gnsampao@gmail.com>
2024-04-12 13:22:40 +0300

    Created src/assets/images/index.js.

---
+++ src/assets/images/index.js
@@ -0,0 +1,13 @@
+import backgroundPatternDesktop from "./background-pattern-desktop.svg";
+import backgroundPatternMobile from "./background-pattern-mobile.svg";
+import iconMinus from "./icon-minus.svg";
+import iconPlus from "./icon-plus.svg";
+import iconStar from "./icon-star.svg";
+
+export {
+  backgroundPatternDesktop,
+  backgroundPatternMobile,
+  iconMinus,
+  iconPlus,
+  iconStar,
+};
